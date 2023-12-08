1540603e4c3b3ef46a04df68fdc35356d72d4205
Zenoviy Fil <fil.zenoviy@gmail.com>
2023-12-08 20:25:27 +0200

    task-1 complited

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,15 @@
 const categories = document.getElementById('categories');
+const numbOfCategories = categories.children.length;
+console.log(`Number of categories: ${numbOfCategories}`);
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(element => { 
+  const categoryName = element.firstElementChild.textContent;
+  const elementsCount = element.lastElementChild.children.length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${elementsCount}`);
+});
+
+
