0acd9b04bb397bbd132159b5b8191153b7215431
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2023-12-11 17:09:57 +0200

    task-4 finish

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form')
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const login = event.target.elements.email.value.trim();
+    const password = event.target.elements.password.value.trim();
+    const userData = {};
+
+    if (login === "" || password === "") {
+        return alert(`All fields must be filled in`)
+    };
+
+    userData.email = login;
+    userData.password = password;
+    console.log(userData);
+
+    form.reset();
+});
 
