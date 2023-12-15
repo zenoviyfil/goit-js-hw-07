0f60e5f3c4e84d404bda630b2350c0279864ef9c
Zenoviy Fil <fil.zenoviy@gmail.com>
2023-12-15 13:43:03 +0200

    update

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,38 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const controls = document.getElementById('controls');
+const boxes = document.getElementById('boxes')
+
+const input = controls.firstElementChild;
+const amount = input.value;
+let box = '';
+let size = 30;
+
+
 function createBoxes(amount) {
- 
+   for (let i = 1; i <= input.value; i++) {
+    box = `<div width=${size}px height=${size}px background-color=${getRandomHexColor()}></div>`;
+    size += 10;
+   }
+  boxes.insertAdjacentHTML('afterbegin', box);
 }
 
-const createBtn = document.querySelector('button[data-action="create"]');
-const destroyBtn = document.querySelector('button[data-action="destroy"]');
+ createBtn.addEventListener('click', () => {
+   if (amount > 0 && amount <= 100) {
+     createBoxes(amount);
+   }
+ });
 
+ destroyBtn.addEventListener('click', () => {
+  input.value = '';
+  boxes.innerHTML = '';
+ })
+
+
+ console.dir(controls)
+ console.log(box)
+ console.log(createBoxes())
+ 
